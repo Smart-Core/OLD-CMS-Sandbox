991c9ed9283978c39573188d1a45a93c8ec99761
Smart-Core <artem.digi@gmail.com>
2013-05-16 01:04:03 +0700

    more flexible autoloader

--- app/autoload.php
+++ app/autoload.php
@@ -1,18 +1,26 @@
 <?php
+if (version_compare(PHP_VERSION, '5.4', '>=') && gc_enabled()) {
+    // Disabling Zend Garbage Collection to prevent segfaults with PHP5.4+
+    // https://bugs.php.net/bug.php?id=53976
+    gc_disable();
+}
+
 /**
  * @var $loader \Composer\Autoload\ClassLoader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 // Кеширование автозагрузчика.
-$loader_prefix = 'sf2.';
 if (function_exists('apc_store') and ini_get('apc.enabled')) {
-    $loader = new \Symfony\Component\ClassLoader\ApcClassLoader($loader_prefix, $loader);
-    $loader->register(true);
-} else if (function_exists('wincache_ucache_set') and class_exists('\Symfony\Component\ClassLoader\WinCacheClassLoader')) {
-    $loader = new \Symfony\Component\ClassLoader\WinCacheClassLoader($loader_prefix, $loader);
-    $loader->register(true);
+    $loader = new \Symfony\Component\ClassLoader\ApcClassLoader(md5(__FILE__), $loader);
+} else if (function_exists('wincache_ucache_set')) {
+    $loader = new \Symfony\Component\ClassLoader\WinCacheClassLoader(md5(__FILE__), $loader);
+} else if ((PHP_SAPI != 'cli' || (isset($_SERVER['DOCUMENT_ROOT']) && isset($_SERVER['REQUEST_URI'])))
+    and function_exists('xcache_set') and (int) ini_get('xcache.var_size') > 0
+    ) {
+    $loader = new \Symfony\Component\ClassLoader\XcacheClassLoader(md5(__FILE__), $loader);
 }
+$loader->register(true);
 
 // intl
 if (!function_exists('intl_get_error_code')) {
