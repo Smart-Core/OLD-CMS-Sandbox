c1f12ddf17a05bb4e606bf68a6c6eb8db17c18e7
Smart-Core <artem.digi@gmail.com>
2013-12-18 22:04:36 +0700

    fix deprecated FOSUserBundle/Entity/User.php

--- src/SmartCore/Bundle/FOSUserBundle/Entity/User.php
+++ src/SmartCore/Bundle/FOSUserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 
 namespace SmartCore\Bundle\FOSUserBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
