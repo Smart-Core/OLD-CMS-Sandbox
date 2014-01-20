e5ea4d13245cbcc61c69a4b7c6ad68597376bfc1
Smart-Core <artem.digi@gmail.com>
2014-01-20 13:10:21 +0700

    small refactoring.

--- src/SmartCore/Module/News/Controller/NewsAdminController.php
+++ src/SmartCore/Module/News/Controller/NewsAdminController.php
@@ -2,7 +2,6 @@
 
 namespace SmartCore\Module\News\Controller;
 
-use SmartCore\Bundle\CMSBundle\Response;
 use SmartCore\Bundle\CMSBundle\Module\Controller;
 use SmartCore\Module\News\Form\Type\NewsFormType;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,42 +25,46 @@ class NewsAdminController extends Controller
         if ($request->isMethod('POST')) {
             $form->submit($request);
             if ($form->isValid()) {
-                $item = $form->getData();
-
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($item);
-                $em->flush($item);
-
-                $this->get('session')->getFlashBag()->add('notice', 'Новость создана.'); // @todo translate
-                return $this->redirect($this->generateUrl('smart_news_admin'));
+                return $this->saveItem($form->getData(), 'smart_news_admin', 'Новость создана.');
             }
         }
-        return $this->render('NewsModule:Admin:create.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render('NewsModule:Admin:create.html.twig', ['form' => $form->createView()]);
     }
 
     public function editAction(Request $request, $id)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $form = $this->createForm(new NewsFormType(), $em->find('NewsModule:News', $id));
-        $form->add('update', 'submit', ['label' => 'Сохранить', 'attr' => ['class' => 'btn btn-primary']]);
+        $form = $this->createForm(new NewsFormType(), $this->getDoctrine()->getManager()->find('NewsModule:News', $id));
+        $form->add('update', 'submit', [
+            'label' => 'Сохранить',
+            'attr' => ['class' => 'btn btn-primary'],
+        ]);
 
         if ($request->isMethod('POST')) {
             $form->submit($request);
             if ($form->isValid()) {
-                $item = $form->getData();
-                $em->persist($item);
-                $em->flush($item);
-
-                $this->get('session')->getFlashBag()->add('notice', 'Новость сохранена.'); // @todo translate
-                return $this->redirect($this->generateUrl('smart_news_admin'));
+                return $this->saveItem($form->getData(), 'smart_news_admin', 'Новость сохранена.');
             }
         }
 
-        return $this->render('NewsModule:Admin:edit.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render('NewsModule:Admin:edit.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * Сохранение записи.
+     *
+     * @param int           $item
+     * @param string        $redirect_to
+     * @param string|null   $flash_notice
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    protected function saveItem($item, $redirect_to, $flash_notice = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($item);
+        $em->flush($item);
+
+        $this->get('session')->getFlashBag()->add('notice', $flash_notice);
+
+        return $this->redirect($this->generateUrl($redirect_to));
     }
 }
