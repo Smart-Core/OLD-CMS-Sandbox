b5cd2a648b37c55673d3b33a95d40b5fb5cec9e4
Smart-Core <artem.digi@gmail.com>
2014-01-22 21:55:52 +0700

    Remove files count from profiler.

--- src/SmartCore/Bundle/SimpleProfilerBundle/Profiler.php
+++ src/SmartCore/Bundle/SimpleProfilerBundle/Profiler.php
@@ -232,11 +232,11 @@ class Profiler
 
         echo 'Time: <b>', round($exec_time, $precision) * 1000 , '</b> ms',
         '. Memory <b>', round((memory_get_usage() - self::$globalStartMemory) / 1024 / 1024, 2), '</b> MB (<b>',
-        round((memory_get_peak_usage(true) - self::$globalStartMemory) / 1024 / 1024, 2), '</b> peak).',
-            ' Files: <b>' . count(get_included_files()) . "</b>.\n";
+        round((memory_get_peak_usage(true) - self::$globalStartMemory) / 1024 / 1024, 2), '</b> peak).';
+            //' Files: <b>' . count(get_included_files()) . "</b>.\n";
 
         if (!is_null(self::$dbLogger) and is_object(self::$dbLogger)) {
-            echo 'DB: <b>' . self::$dbLogger->currentQuery . '</b>';
+            echo ' DB: <b>' . self::$dbLogger->currentQuery . '</b>';
 
             $queries_time = 0;
             foreach (self::$dbLogger->queries as $value) {
