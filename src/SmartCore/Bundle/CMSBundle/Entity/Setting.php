646cd72c11ff5277d5c97c0ccf9d2e8ebd0ba7c8
Smart-Core <artem.digi@gmail.com>
2014-07-06 02:46:40 +0700

    settings entity

---
+++ src/SmartCore/Bundle/CMSBundle/Entity/Setting.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace SmartCore\Bundle\CMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="engine_settings",
+ *      indexes={
+ *          @ORM\Index(name="bundle", columns={"bundle"}),
+ *          @ORM\Index(name="key_name", columns={"key_name"}),
+ *      }
+ * )
+ */
+class Setting
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=32, nullable=false)
+     * @Assert\NotBlank()
+     */
+    protected $bundle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", name="key_name", length=64, nullable=false)
+     * @Assert\NotBlank()
+     */
+    protected $key;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $value;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(type="boolean")
+     */
+    protected $serialized;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->serialized = false;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $bundle
+     * @return $this
+     */
+    public function setBundle($bundle)
+    {
+        $this->bundle = $bundle;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBundle()
+    {
+        return $this->bundle;
+    }
+
+    /**
+     * @param string $key
+     * @return $this
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $value
+     * @return $this
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param boolean $serialized
+     * @return $this
+     */
+    public function setSerialized($serialized)
+    {
+        $this->serialized = $serialized;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getSerialized()
+    {
+        return $this->serialized;
+    }
+}
