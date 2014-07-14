5698797ad81dec9e2aaba963c71faf7bd86db5fe
Smart-Core <artem.digi@gmail.com>
2014-07-14 19:23:30 +0700

    styles editor

--- src/SmartCore/Bundle/CMSBundle/Controller/AdminAppearanceController.php
+++ src/SmartCore/Bundle/CMSBundle/Controller/AdminAppearanceController.php
@@ -16,6 +16,7 @@ class AdminAppearanceController extends Controller
     public function indexAction()
     {
         return $this->render('CMSBundle:AdminAppearance:index.html.twig', [
+            'styles'    => $this->getStyles(),
             'templates' => $this->getTemplates(),
         ]);
     }
@@ -60,6 +61,35 @@ class AdminAppearanceController extends Controller
     }
 
     /**
+     * @param Request $request
+     * @param string $name
+     * @return Response|RedirectResponse
+     */
+    public function styleEditAction(Request $request, $name)
+    {
+        $style_file_path = $this->container->getParameter('kernel.root_dir') . '/../web' . $this->get('cms.config')->get('cms', 'appearance_styles_path') . $name;
+
+        if (!file_exists($style_file_path)) {
+            return $this->redirectToRoute('cms_admin_appearance');
+        }
+
+        $style_code = file_get_contents($style_file_path);
+
+        if ($request->isMethod('POST')) {
+            $style_code = $request->request->get('style_code');
+
+            $this->addFlash('error', '<b>@todo</b> пока не рабоатет сохранение');
+            //return $this->redirectToRoute('cms_admin_appearance_template', ['name' => $name]);
+        }
+
+        return $this->render('CMSBundle:AdminAppearance:style_edit.html.twig', [
+            'name'          => $name,
+            'styles'        => $this->getStyles(),
+            'style_code'    => $style_code,
+        ]);
+    }
+
+    /**
      * @return array
      */
     protected function getTemplates()
@@ -74,5 +104,22 @@ class AdminAppearanceController extends Controller
         }
 
         return $templates;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getStyles()
+    {
+        $finder = new Finder();
+        $finder->files()->sortByName()->name('*.css')->name('*.less')->in($this->container->getParameter('kernel.root_dir') . '/../web' . $this->get('cms.config')->get('cms', 'appearance_styles_path'));
+
+        $styles = [];
+        /** @var \Symfony\Component\Finder\SplFileInfo $file */
+        foreach ($finder as $file) {
+            $styles[] = $file->getFilename();
+        }
+
+        return $styles;
     }
 }
