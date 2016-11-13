2824dc929e963e8400572ae3eb27abef055d534a
Smart-Core <artem.digi@gmail.com>
2016-11-14 06:21:27 +0700

    fixes

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,5 +1,6 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
@@ -16,6 +17,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 $loader = require __DIR__.'/../app/autoload.php';
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
