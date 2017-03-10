00862cbc4e81581199e15ea598d43b1b31b534fd
Smart-Core <artem.digi@gmail.com>
2017-03-10 23:46:23 +0700

    imagine filter fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends CMSAppKernel
     {
         $bundles = array(
             new Dizda\CloudBackupBundle\DizdaCloudBackupBundle(),
-            new YamilovS\SypexGeoBundle\YamilovsSypexGeoBundle(),
             new SmartCore\Bundle\AcceleratorCacheBundle\AcceleratorCacheBundle(),
             new SmartCore\Bundle\DbDumperBundle\SmartDbDumperBundle(),
             new SmartCore\Bundle\SitemapBundle\SmartSitemapBundle(),
